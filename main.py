3dc96313b03089ea199d8ba47b68c9a2032d597f
Shane McDonough <shanemcdman@gmail.com>
2020-08-09 22:03:51 -0700

    Set up parse_args better

--- main.py
+++ main.py
@@ -7,19 +7,34 @@ def print_command_template() -> None:
     print("\t\t-f - input is a filename to translate; next word is file name")
     print("\t\t-o - Use an output file; next word is file name")
 
-def parse_args(args: list) -> (int, bool, str):
-    try:
-        if read_from_file := '-f' in args:
-            args.remove('-f')
-        _ = args.pop(0)
-        print(_)
-        if len(args) > 2:
-        return int(args[0]), read_from_file, args[1],
-    except Exception as e:
-        print("Error: " + e)
+def parse_args(args: list) -> [int, str, bool, str]:
+    result = []
+    output_file = ""
+    read_from_file = False
+    next_arg_is_output_file = False
+    _ = args.pop(0)
+    for arg in args:
+        if arg == '-o':
+            next_arg_is_output_file = True
+        elif arg == '-f':
+            read_from_file = True
+        elif next_arg_is_output_file:
+            output_file = arg
+        else:
+            result.append(arg)
+    result += [read_from_file, output_file]
+    # if len(result) > 4:
+    #     raise "Too many args"
+    # elif len(result) < 4:
+    #     raise "Not enough args"
+    return result
 
 def main():
-    print(parse_args(sys.argv))
+    try:
+        print(parse_args(sys.argv))
+    except Exception as e:
+        print("Error:", e)
+        print_command_template()
 
 if __name__ == "__main__":
     main()
