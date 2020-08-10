c74fa7f0c4476aeb26da48994108e4470de01cec
Shane McDonough <shanemcdman@gmail.com>
2020-08-09 21:45:54 -0700

    Add print_command_template and parse_args

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import string
 import sys
 
+def print_command_template() -> None:
+    print("ROTN {n; how much rotation} {text to be translated or name of file} [flag]")
+    print("\tflags:")
+    print("\t\t-f - input is a filename to translate; next word is file name")
+    print("\t\t-o - Use an output file; next word is file name")
+
+def parse_args(args: list) -> (int, bool, str):
+    try:
+        if read_from_file := '-f' in args:
+            args.remove('-f')
+        _ = args.pop(0)
+        print(_)
+        if len(args) > 2:
+        return int(args[0]), read_from_file, args[1],
+    except Exception as e:
+        print("Error: " + e)
+
 def main():
-    print(sys.argv)
+    print(parse_args(sys.argv))
 
 if __name__ == "__main__":
     main()
